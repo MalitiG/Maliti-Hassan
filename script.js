b61d90837da18afa691c4a87c1825555f1acd988
MalitiG <malitihassan98@gmail.com>
2025-06-20 12:55:21 +0300

    added  formspree

--- script.js
+++ script.js
@@ -42,3 +42,33 @@ function erase() {
 document.addEventListener("DOMContentLoaded", function () {
   if (textArray.length) setTimeout(type, newTextDelay);
 });
+
+window.addEventListener("submit", function (e) {
+  alert("Thank you! Your message has been sent.");
+}, false);
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("#contact form");
+
+    form.addEventListener("submit", async function (e) {
+        e.preventDefault();
+
+        const formData = new FormData(form);
+
+        const response = await fetch(form.action, {
+            method: form.method,
+            body: formData,
+            headers: { 'Accept': 'application/json' }
+        });
+
+        if (response.ok) {
+            form.reset();
+            alert("Thank you! Your message has been sent.");
+        } else {
+            alert("Oops! There was a problem sending your message.");
+        }
+    });
+});
+
+
